7c9897dccaf8f04a4f73c50e55b7ac6d22365c08
mctursh <adenugaayoadehenry63@gmail.com>
2025-12-05 15:42:51 +0100

    Implement cleanup method for PoolEntry and enhance BufferPool methods for page management, including pinning and eviction logic.

--- src/buffer/pool.rs
+++ src/buffer/pool.rs
@@ -31,6 +31,12 @@ impl Default for PoolEntry {
     }
 }
 
+impl PoolEntry {
+    pub fn cleanup (&mut self) {
+        self.pin_count -= 1;
+    }
+}
+
 pub struct BufferPool {
     entries: [PoolEntry; DEFAULT_BUFFER_POOL_SIZE],
     access_counter: u64, // increments on each access, used instead of timestamps.
@@ -44,8 +50,8 @@ impl BufferPool {
         })
     }
 
-    pub fn find_page (page_id: u32, pool: &BufferPool) -> PoolPageReuslt<&PoolEntry> {
-       let page =  pool.entries.iter().find(|&entry| {
+    pub fn find_page (page_id: u32, pool: &mut BufferPool) -> PoolPageReuslt<&mut PoolEntry> {
+       let page =  pool.entries.iter_mut().find(|entry| {
             if let Some(page_number) = entry.page_id {
                 page_number == page_id
             } else {
@@ -53,15 +59,56 @@ impl BufferPool {
             }
         });
         //TODO: add pin logic to this function and impl an unpin upon drop
-        Ok(page)
+        if let Some(actual_page) = page {
+            actual_page.pin_count += 1;
+            Ok(Some(actual_page))
+        } else {
+            Ok(None)
+        }
+
     }
 
-    pub fn find_empty_slot () -> PoolResult<()> {
-        Ok(())
+    pub fn find_empty_slot (pool: &mut BufferPool) -> PoolResult<(&mut PoolEntry)> {
+        let mut empty_slot: Option<usize> = None;
+        // let mut empty_slot: Option<&mut PoolEntry> = None;
+        for (index, entry) in pool.entries.iter().enumerate()    
+        {
+            if let None = entry.page_id {
+                empty_slot = Some(index);
+                break
+            } else {
+                continue;
+            }
+        };
+
+        if let Some(slot) = empty_slot {
+            //at this point we are sure we found a free page in the bufferPool
+            pool.entries[slot].pin_count += 1; // increase pin count
+            Ok(&mut pool.entries[slot])
+        } else {
+            // this mean we didn't find find an empty Slot in the BufferPool then we need to generate
+            Self::evict_lru(pool)
+        }
     }
 
-    pub fn evict_lru () -> PoolResult<()> {
-        Ok(())
+    pub fn evict_lru (pool: &mut BufferPool) -> PoolResult<&mut PoolEntry> {
+        let mut least_last_accessed_value = 0_u64;
+        let mut evicted_pool: Option<&mut PoolEntry> = None;
+        for entry in pool.entries.iter_mut()
+            .filter(|entry | entry.pin_count == 0) // returns nly pages with Zero pin count i.e being used in the code currently.
+        {
+            if entry.last_accessed <= least_last_accessed_value {
+                least_last_accessed_value = entry.last_accessed;
+                evicted_pool = Some(entry)
+            }
+        };
+        if let Some(pool) = evicted_pool {
+            pool.pin_count += 1; // increase pin count
+            return Ok(pool);
+        } else {
+            //TODO: sample error, should fix to appropriate error, All pages are currenlt in use will need to expand bufferPool
+            return Err(DbError::PageFull { page_id: 2 })
+        }
     }
 }
 mod pool {
